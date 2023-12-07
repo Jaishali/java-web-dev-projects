fd3965758b5985c5cb183313ff60dbb262e554b8
Jaishali <142702371+Jaishali@users.noreply.github.com>
2023-12-07 10:29:09 -0600

    no change

--- classes-part-one/exercises/src/main/java/Student.java
+++ classes-part-one/exercises/src/main/java/Student.java
@@ -1,8 +1,135 @@
+import java.util.ArrayList;
+
 public class Student {
         private String name;
         private int studentId;
-        private int numberOfCredits = 0;
-        private double gpa = 0.0;
+        private  int numberOfCredits = 0;
+        private  double gpa = 0.0;
+
+    public static class Course {
+        private String topic;
+        private String instructor;
+        private int enrolledStudents;
+
+
+        public Course(String topic, String instructor,int enrolledStudents ){
+              this.topic = topic;
+              this.instructor = instructor;
+              this.enrolledStudents = enrolledStudents;
+
+}
+        public void setTopic(String topic) {
+            this.topic = topic;
+        }
+        public void setInstructor(String instructor) {
+            this.instructor = instructor;
+        }
+        public void setEnrolledStudents(int enrolledStudents) {
+            this.enrolledStudents = enrolledStudents;
+        }
+
+        public String getTopic() {
+            return topic;
+        }
+        public String getInstructor() {
+            return instructor;
+        }
+        public int getEnrolledStudents(){
+            return enrolledStudents;
+        }
+    }
+
+    public static class Teacher {
+        private String firstName;
+        private String lastName;
+        private int subject;
+        private double yearsTeaching;
+
+
+        public Teacher(String firstName ,String lastName, int subject, double yearsTeaching){
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.subject = subject;
+            this.yearsTeaching = yearsTeaching;
+        }
+
+        public void setFirstName(String firstName) {
+            this.firstName = firstName;
+        }
+        public void setLastName(String lastName) {
+            this.lastName = lastName;
+        }
+        public void setSubject(int subject) {
+            this.subject = subject;
+        }
+        public void setYearsTeaching(double yearsTeaching){
+            this.yearsTeaching = yearsTeaching;
+        }
+
+        public String getFirstName() {
+            return firstName;
+        }
+        public String getLastName() {
+            return  lastName;
+        }
+        public int getSubject() {
+            return subject;
+        }
+        public double getYearsTeaching() {
+            return yearsTeaching;
+        }
+
+
+    }
+        public Student(String name, int studentId, int numberOfCredits, double gpa) {
+                this.name = name;
+                this.studentId = studentId;
+                this.numberOfCredits = numberOfCredits;
+                this.gpa = gpa;
+        }
+
+      public Student(String name, int studentId) {
+                this.name = name;
+                this.studentId = studentId;
+                this.numberOfCredits = 0;
+                this.gpa = 0.0;
+        }
+
+
+        public void setName(String name) {
+                this.name = name;
+        }
+
+        public void setStudentId(int studentId) {
+                this.studentId = studentId;
+        }
+
+        public void setGpa(double gpa) {
+                this.gpa = gpa;
+        }
+
+        private void setNumberOfCredits(int numberOfCredits) {
+                this.numberOfCredits = numberOfCredits;
+        }
+
+        public String getName() {
+                return name;
+        }
+
+        public int getStudentId() {
+                return studentId;
+        }
+
+        public int getNumberOfCredits() {
+                return numberOfCredits;
+        }
+
+        public double getGpa() {
+                return gpa;
+        }
+
+
+
 
         // Drop your getters and setters below for the Student class.
         // To instantiate the Student class, add your code to the main in the file, SchoolPractice.
--- classes-part-one/exercises/src/main/java/StudentPractice.java
+++ classes-part-one/exercises/src/main/java/StudentPractice.java
@@ -1,5 +1,7 @@
 public class StudentPractice {
-    public static void main(String[] args){
-        //insantiate your Student class below
+    public static void main(String[] args) {
+        //instantiate your Student class below
+        Student student = new Student("your name", 12345, 1, 4.0);
+        System.out.println(student);
     }
 }
