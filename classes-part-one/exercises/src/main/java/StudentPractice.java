09f403bc5219f8976cf33a4e8ebae750db0d1f3e
Jaishali <142702371+Jaishali@users.noreply.github.com>
2023-12-07 18:03:55 -0600

    no change

--- classes-part-one/exercises/src/main/java/StudentPractice.java
+++ classes-part-one/exercises/src/main/java/StudentPractice.java
@@ -1,7 +1,11 @@
 public class StudentPractice {
     public static void main(String[] args) {
         //instantiate your Student class below
-        Student student = new Student("your name", 12345, 1, 4.0);
-        System.out.println(student);
+        Student student = new Student("Jaishali", 12345, 1, 4.0);
+        System.out.println(student.getName());
+        System.out.println(student.getStudentId());
+        System.out.println(student.getNumberOfCredits());
+        System.out.println(student.getGpa());
+
     }
 }
