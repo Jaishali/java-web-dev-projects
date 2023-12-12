eed173256abb508d73d3a92efe653219e41d291b
Jaishali <142702371+Jaishali@users.noreply.github.com>
2023-12-11 20:08:19 -0600

    no change

--- classes-part-2/studio/src/main/java/org/launchcode/Menu.java
+++ classes-part-2/studio/src/main/java/org/launchcode/Menu.java
@@ -1,5 +1,6 @@
 package org.launchcode;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.Date;
 
@@ -11,6 +12,48 @@ public class Menu {
         this.lastUpdated = d;
         this.items = i;
     }
+
+    public Menu() {
+        
+    }
+
+
+    public String toString() {
+        StringBuilder appetizers = new StringBuilder();
+        MenuItem[] menuItems = new MenuItem[0];
+        for (MenuItem item : menuItems) {
+            if (item.getCategory().equals("appetizers")) {
+                appetizers.append("\n").append(item.toString()).append("\n");
+            }
+        }
+
+        StringBuilder mainCourses = new StringBuilder();
+        for (MenuItem item : menuItems) {
+            if (item.getCategory().equals("main courses")) {
+                mainCourses.append("\n").append(item.toString()).append("\n");
+            }
+        }
+        StringBuilder desserts = new StringBuilder();
+        for (MenuItem item : menuItems) {
+            if (item.getCategory().equals("desserts")) {
+                desserts.append("\n").append(item.toString()).append("\n");
+            }
+        }
+
+        return "\nTONY'S PIZZA MENU" +
+                "APPETIZERS" + appetizers.toString() + "\n" +
+                "MAIN COURSES" + mainCourses.toString() + "\n" +
+                "DESSERTS" + desserts.toString() + "\n";
+    }
+  /*  void addItem(MenuItem newItem){
+        menuItems.add(newItem);
+        lastUpdated = LocalDate.now();
+    }
+    void removeItem(MenuItem item){
+        menuItems.remove(item);
+        lastUpdated = LocalDate.now();
+    }*/
+
 
     public void setLastUpdated(Date lastUpdated) {
         this.lastUpdated = lastUpdated;
@@ -26,6 +69,9 @@ public class Menu {
 
     public ArrayList<MenuItem> getItems() {
         return items;
+    }
+
+    public void addItem(MenuItem item1) {
     }
 }
 
