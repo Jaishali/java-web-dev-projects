d74d540d3b9831c4603c97a1e08def7c44483b39
Jaishali <142702371+Jaishali@users.noreply.github.com>
2024-01-04 20:20:30 -0600

    no change

--- interfaces/studio/spinning-discs-studio/src/main/java/org/launchcode/CD.java
+++ interfaces/studio/spinning-discs-studio/src/main/java/org/launchcode/CD.java
@@ -1,8 +1,29 @@
 package org.launchcode;
 
-public class CD {
-    // TODO: Implement your custom interface.
+public class CD extends Media {
+    private boolean isMusicCD;
 
-    // TODO: Determine which fields, methods, and constructors can be extended from the base class and which ones
-    //  need to be declared separately.
+
+    public CD(String name,boolean isMusicCD) {
+        super(name, discType:"", spinRate:"800", capacity:"700");
+        this.isMusicCD = isMusicCD;
+        setDiscType(isMusicCD ? "music CD" : "CD-RW");
+    }
+    public CD(String name){
+        this(name,isMusicCD:false);
+    }
+
+    public void setMusicCD(boolean musicCD) {
+        isMusicCD = musicCD;
+    }
+
+    public boolean isMusicCD() {
+        return isMusicCD;
+    }
+
+    @Override
+    public String toString() {
+        String header = isMusicCD ? "Tracks ": "Files";
+        return super.toString() +getFormattedFileList(header);
+    }
 }
