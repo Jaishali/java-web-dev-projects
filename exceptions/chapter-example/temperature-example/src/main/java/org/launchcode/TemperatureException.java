6a6c983c0ea579a429f01271cb1b37357baad5d1
Jaishali <142702371+Jaishali@users.noreply.github.com>
2024-01-10 20:17:24 -0600

    no change

--- exceptions/chapter-example/temperature-example/src/main/java/org/launchcode/TemperatureException.java
+++ exceptions/chapter-example/temperature-example/src/main/java/org/launchcode/TemperatureException.java
@@ -1,5 +1,19 @@
 package org.launchcode;
 
-public class TemperatureException {
+public class TemperatureException extends Exception  {
     // Write code here!
+    private double aFahrenheit;
+    private double absoluteZeroFahrenheit;
+    public TemperatureException(String message) {
+        super(message);
+
+
+        if (aFahrenheit < absoluteZeroFahrenheit) {
+            try {
+                throw new TemperatureException("That temperature is too low!");
+            } catch (TemperatureException e) {
+                e.printStackTrace();
+            }
+        }
+    }
 }
